bab33ba8759d8b247892e03ab32fdb11aa5ccbf1
ailu-torreval <ailu.torreval@gmail.com>
2023-10-23 14:14:43 +0200

    clone from github

--- sockets/index.js
+++ sockets/index.js
@@ -21,5 +21,5 @@ io.on('connection', (socket) => {
 var port = process.env.PORT || 3001;
 
 server.listen(port, function(){
-   console.log('listening in http://localhost:' + port);
+   console.log('listen listening in http://localhost:' + port);
 });
